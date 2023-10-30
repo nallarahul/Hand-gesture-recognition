6abcbbca17cc798aa8c0a3ee1d07a4e1a9662878
Nalla Rahul <114102765+nallarahul@users.noreply.github.com>
2023-10-30 11:29:06 +0530

    added files

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app=express();
+const port=3000;
+const raw_data={"name":"Rahul"};
+app.get("/",(req,res)=>{
+    res.render("index.ejs",{data:raw_data.name});
+})
+app.listen(port,()=>{
+    console.log(`Server running on port ${port}`);
+});

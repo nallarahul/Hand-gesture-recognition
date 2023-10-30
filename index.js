fb31269cb73408f9da9150d273009d048ba31318
Nalla Rahul <114102765+nallarahul@users.noreply.github.com>
2023-10-30 13:06:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 import express from "express";
 const app=express();
 const port=3000;
-const raw_data= {
-        "finger_one": finge_one,
-        "finger_two": finger_two,
-        "finger_three": finger_three,
-        "finger_four": finger_four,
-        "finger_five": finger_five
-    };
+const raw_data= {"name": "Rahul"};
 app.get("/",(req,res)=>{
     res.render("index.ejs",{data:raw_data.name});
 })
